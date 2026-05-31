360520291692059642ed699c7d3cde314e0a714e
nikhil shendre <nikhilshendre008@gamil.com>
2026-05-31 23:18:46 +0530

    Update AppContext with backend connection

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,25 +2,79 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets";
 
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { doctors as localDoctors } from "../assets/assets";
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "₹";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [doctors, setDoctors] = useState(localDoctors);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : false
+  );
+  const [userData, setUserData] = useState(false);
 
-    const currencySymbol = '$'
-
-    const value = {
-        doctors,
-        currencySymbol
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success && data.doctors.length > 0) {
+        setDoctors(data.doctors);
+      } else {
+        setDoctors(localDoctors);
+      }
+    } catch (error) {
+      console.log(error);
+      setDoctors(localDoctors);
     }
+  };
 
-    return(
-        
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: { token },
+      });
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
 
-export default AppContextProvider
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
+
+  const value = {
+    doctors,
+    getDoctorsData,
+    currencySymbol,
+    backendUrl,
+    token,
+    setToken,
+    userData,
+    setUserData,
+    loadUserProfileData,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
